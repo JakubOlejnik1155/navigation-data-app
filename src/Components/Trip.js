afd874c8925dac867bff7aa5cf9eed45adda8f03
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-09-22 00:26:55 +0200

    fix one trip styles

--- src/Components/Trip.js
+++ src/Components/Trip.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { theme } from './../data/styleThemes';
 import DeleteIcon from '@material-ui/icons/DeleteForeverRounded';
+import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import { get, set } from 'idb-keyval';
@@ -14,6 +15,17 @@ const useStyles = makeStyles(() => ({
     },
     deleteIcon:{
         fill: theme.red,
+    },
+    showBtn:{
+        position: 'absolute',
+        bottom: '2px',
+        left: '2px',
+        margin: '2px',
+        padding: '2px',
+        fontSize: '12px',
+        color: theme.blue,
+        borderColor: theme.blue,
+        textTransform: 'lowercase'
     }
 }));
 
@@ -22,6 +34,7 @@ const TripContainer = styled.div`
     background-color: transparent;
     color: ${props => props.state.isNightModeOn ? theme.red : theme.dark};
     width: 100%;
+    min-height: 80px;
     margin-top: 10px;
     overflow: hidden;
     border: 2px solid ${props => props.state.isNightModeOn ? theme.red : theme.dark};
@@ -32,8 +45,11 @@ const TripContainer = styled.div`
 const Distance = styled.span`
     font-family: 'Poppins', sans-serif;
     font-weight: bold;
-    font-size: 18px;
-    float: right;
+    font-size: 24px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 `;
 function msToTime(milliseconds) {
     var day, hour, minute, seconds;
@@ -73,12 +89,15 @@ const Trip = ({ trip, state }) => {
     return (
         <TripContainer state={state}>
             <span style={{fontWeight: 'light', fontStyle: 'italic'}}>{new Date(trip.startTime).toLocaleDateString()}</span>
-            <p>{
+            <span style={{position: 'absolute', bottom: '2px', right: '2px'}}>{
                     msToTime(new Date(trip.endTime).getTime() - new Date(trip.startTime).getTime()).hour + "h " +
                     msToTime(new Date(trip.endTime).getTime() - new Date(trip.startTime).getTime()).minute + "m "+
                     msToTime(new Date(trip.endTime).getTime() - new Date(trip.startTime).getTime()).seconds + "s "
-                }</p>
+                }</span>
             <Distance>{Math.round(trip.tripDistance / 1852 * 10) / 10} nm</Distance>
+            <Button variant="outlined" size="small" color="primary" className={classes.showBtn}>
+                show more
+            </Button>
             <IconButton aria-label="delete" size="small"className={classes.deleteBtn} onClick={()=>{deleteTrip(trip)}}>
                 <DeleteIcon fontSize="small" className={classes.deleteIcon}/>
             </IconButton>
