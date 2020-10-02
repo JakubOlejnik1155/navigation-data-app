235a516785b488a7dccafe00679c584578aab527
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-10-02 08:36:04 +0200

    change breakpoints port to 3000

--- src/data/breakpoints.js
+++ src/data/breakpoints.js
@@ -1,3 +1,3 @@
 // TODO: change breakpoints from localhost to ip addres
-export const SERVER = 'http://localhost:5000/api/user';
-export const API = 'http://localhost:5000/api';
+export const SERVER = 'http://localhost:3000/api/user';
+export const API = 'http://localhost:3000/api';
